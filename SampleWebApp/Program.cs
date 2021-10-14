be5cb1a3aa743831607c054ba010dd4f20329475
joelAbulencia <joel.a.p.abulencia@gmail.com>
2021-10-15 01:08:18 +0800

    Update Program.cs
    Commit fix

--- SampleWebApp/Program.cs
+++ SampleWebApp/Program.cs
@@ -14,7 +14,7 @@ namespace SampleWebApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        //}
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
